84aaca0797fc8e292447d5e700483dea9b6b12f3
François Sarradin <fsarradin@gmail.com>
2018-12-11 10:53:10 +0100

    fix person finding bug in DB repository

--- src/main/scala/io/univalence/aboutfp/repository/PersonDbRepository.scala
+++ src/main/scala/io/univalence/aboutfp/repository/PersonDbRepository.scala
@@ -3,8 +3,8 @@ package io.univalence.aboutfp.repository
 import cats.effect.IO
 import doobie._
 import doobie.implicits._
-
 import io.univalence.aboutfp.Person
+import scala.util.Try
 
 class PersonDbRepository(xa: Transactor[IO]) extends PersonRepository {
   override def allPersons: IO[List[Person]] =
@@ -13,11 +13,17 @@ class PersonDbRepository(xa: Transactor[IO]) extends PersonRepository {
       .to[List]
       .transact(xa)
 
-  override def person(id: String): IO[Option[Person]] =
-    sql"SELECT id, name, age FROM person WHERE id = ${id.toInt}"
-      .query[Person]
-      .option
-      .transact(xa)
+  override def person(id: String): IO[Option[Person]] = {
+    val maybeId: Option[Int] = Try { id.toInt }.toOption
+
+    if (maybeId.isDefined)
+      sql"SELECT id, name, age FROM person WHERE id = ${maybeId.getOrElse(0)}"
+        .query[Person]
+        .option
+        .transact(xa)
+    else
+      IO(None)
+  }
 
   override def count: IO[Int] =
     sql"SELECT COUNT(id) FROM person"
