a71014038dca94350cc3a9dbfd4232f896f3c10b
François Sarradin <fsarradin@gmail.com>
2018-12-11 21:29:09 +0100

    add a demo

--- src/main/scala/io/univalence/aboutfp/tool/TestMain.scala
+++ src/main/scala/io/univalence/aboutfp/tool/TestMain.scala
@@ -1,13 +1,13 @@
 package io.univalence.aboutfp.tool
 
 import cats.effect._
-
+import scala.io.Source
 import scala.util.Random
 
 object TestMain {
 
   def main(args: Array[String]): Unit = {
-    println("hello")
+
   }
 
 }
